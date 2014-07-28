b3730674277b533d165747a60fd043b7c0828045
David Whitlock <alovedalongthe@gmail.com>
2014-07-28 21:12:25 +0700

    Tidied up by using pipe operator

--- lib/mensagem/quotes.ex
+++ lib/mensagem/quotes.ex
@@ -5,13 +5,14 @@ defmodule Mensagem.Quotes do
     quote_dir = Path.join(__DIR__, "quotes")
     file_list = File.ls! quote_dir
     :random.seed(:erlang.now())
-    quote_file = Path.join(quote_dir, :lists.nth(:random.uniform(length file_list), file_list))
-    lines = String.split(File.read!(quote_file), "\n%\n", trim: true)
-    :lists.nth(:random.uniform(length lines), lines)
+    quote_file = Path.join(quote_dir,
+                length(file_list) |> :random.uniform |> :lists.nth(file_list))
+    lines = File.read!(quote_file) |> String.split("\n%\n", trim: true)
+    length(lines) |> :random.uniform |> :lists.nth(lines)
   end
 
   def fetch_remind() do
     rem_path = Path.join(__DIR__, "Reminders.txt")
-    lines = String.split(File.read!(rem_path), "\n", trim: true)
+    lines = File.read!(rem_path) |> String.split("\n", trim: true)
   end
 end
