b82cbe15d12b677685aede4e4e783cfc6fad63c0
David Whitlock <alovedalongthe@gmail.com>
2014-07-31 20:57:23 +0700

    Delete old reminders when adding a new one

--- lib/mensagem/remind.ex
+++ lib/mensagem/remind.ex
@@ -2,17 +2,18 @@ defmodule Mensagem.Remind do
   @moduledoc false
 
   def fetch_remind() do
-    rems = get_remfile |> get_rem(greg_date)
+    rems = get_remfile
 
     case rems do
-      "" -> "No reminders.\n"
-      _ -> "Reminders for today.\n" <> rems
+      [] -> "No reminders.\n"
+      _ -> "Reminders for today.\n" <> get_rem(rems, greg_date)
     end
   end
 
   def add_remind(date, message) do
     greg = date |> String.split("/") |> rem_date |> to_string
-    rems = get_remfile ++ [greg <> "::" <> message] |> Enum.sort(&(&1 > &2))
+    rems = get_remfile ++ [greg <> "::" <> message] |> Enum.sort
+            |> Enum.drop_while(fn x -> split_to_int(x) < greg_date end)
     rem_path = Path.join(__DIR__, "reminders.txt")
     File.write!(rem_path, rems |> Enum.join("\n"))
   end
@@ -23,8 +24,8 @@ defmodule Mensagem.Remind do
   end
 
   defp get_rem(rems, today) do
-    num_days = 0..3 |> Enum.map fn x -> to_string(today + x) end
-    rems |> Enum.filter(fn x -> String.starts_with?(x, num_days) end) |> Enum.join("\n")
+    rems |> Enum.filter(fn x -> split_to_int(x) < today + 4
+          and split_to_int(x) >= today end) |> Enum.join("\n")
   end
 
   defp greg_date(date \\ :erlang.date), do: date |> :calendar.date_to_gregorian_days
@@ -37,6 +38,8 @@ defmodule Mensagem.Remind do
       _ -> greg_date
     end
   end
+
+  defp split_to_int(line), do: String.split(line, "::") |> hd |> String.to_integer
 
   defp to_int(number), do: String.to_integer number
 
